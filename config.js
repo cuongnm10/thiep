b9fce0c317cea79adf3f6a305f4f90d847b78d04
cuongnm10 <109355388+cuongnm10@users.noreply.github.com>
2022-10-19 20:47:46 +0700

    Add files via upload
    create

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Tyter';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100008466582019';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc ấy 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với ấy trong hôm nay và cả những ngày sau.Đừng sợ gì cả tớ sẽ không bỏ cuộc đâu tớ sẽ chờ , chờ đến ngày cậu lại mở lòng đồng ý chàng trai hư này:)) Mãi iu';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
